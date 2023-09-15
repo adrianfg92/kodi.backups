e370194f99d3823a27294016d773c90a24711c53
Adrián <adrian.fernandez@anjanadata.com>
2023-09-16 01:36:38 +0200

    Update to v26

--- addon.py
+++ addon.py
@@ -1,19 +1,34 @@
 import xbmcaddon
 import xbmcgui
 
-from lib.mega import Mega
+from resources.lib.mega import Mega
+
+FOLDERNAME = 'Kodi'
+# FAVOURITES = 'favourites.xml'
 
 addon = xbmcaddon.Addon()
 addonName = addon.getAddonInfo('id')
-mega = Mega()
 
 selfAddon = xbmcaddon.Addon(id=addonName)
-password = selfAddon.getSetting('username')
-username = selfAddon.getSetting('password')
+password = selfAddon.getSetting('password')
+username = selfAddon.getSetting('username')
 
+mega = Mega()
 m = mega.login(username, password)
-quota = m.get_quota()
 
-line1 = "Hello Adrian! " + quota
+# Get folder andcreate folder if not exist
+folder = m.find(FOLDERNAME)
+if not folder:
+    m.create_folder(FOLDERNAME)
+    folder = m.find(FOLDERNAME)
 
+line1 = "Folder id: " + folder[0]
 xbmcgui.Dialog().ok(addonName, line1)
+
+# # Deleting old backup
+# file = m.find(FAVOURITES)
+# if file:
+#     m.destroy(file[0])
+#
+# # Upload new backup
+# m.upload(FAVOURITES, folder[0])
