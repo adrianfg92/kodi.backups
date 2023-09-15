029eb7efc5fd23c1493fd6e78cb2542b7a68b97c
Adrián <adrian.fernandez@anjanadata.com>
2023-09-15 12:51:03 +0200

    Manual

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ echo " 1/2 ==> Setting new version";
 file=`cat addon.xml`
 version=${file##*name=\"kodi.backups\" version=\"0.0.};
 version=${version%% *};
-version=$(printf %.1s "$version");
+version=$(printf %.2s "$version");
 newVersion=$(($version+1))
 
 sed "s/version=\"0.0.$version\"/version=\"0.0.${newVersion}\"/" addon.xml > addon_temp.xml;
@@ -15,9 +15,9 @@ rm addon.xml;
 mv addon_temp.xml addon.xml;
 
 echo " 2/2 ==> Commit and push";
-git add .;
-git commit -am "Update to v${newVersion}";
-git push;
+#git add .;
+#git commit -am "Update to v${newVersion}";
+#git push;
 
 echo "
 
